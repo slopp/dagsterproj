6d445f21bb90d0243a467ea4b82b48eb7315f75f
Sean Lopp <lopp@Seans-MacBook-Pro.local>
2022-10-19 10:32:19 -0600

    GHA reqs a subpath

--- dagsterproj/repository.py
+++ dagsterproj/repository.py
@@ -1,8 +1,13 @@
 from dagster import load_assets_from_package_module, repository, with_resources
 from dagster_dbt import load_assets_from_dbt_project, dbt_cli_resource
+import os
 
-DBT_PROFILES_DIR = "../dbtproj"
-DBT_PROJECT_DIR = "../dbtproj"
+if os.environ["DAGSTER_DEPLOYMENT"] == "local":
+    DBT_PROFILES_DIR = "../dbtproj"
+    DBT_PROJECT_DIR = "../dbtproj"
+else:
+    DBT_PROFILES_DIR = "./dbtproj"
+    DBT_PROJECT_DIR = "./dbtproj"
 
 dbt_assets = load_assets_from_dbt_project(
     project_dir=DBT_PROJECT_DIR,
