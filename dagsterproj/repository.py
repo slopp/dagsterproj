deb816f52abdb8012319ca007a546ad04315c123
Sean Lopp <lopp@Seans-MacBook-Pro.local>
2022-10-25 14:52:41 -0600

    add a job

--- dagsterproj/repository.py
+++ dagsterproj/repository.py
@@ -1,4 +1,4 @@
-from dagster import load_assets_from_package_module, repository, with_resources, asset
+from dagster import AssetSelection, define_asset_job, load_assets_from_package_module, repository, with_resources, asset
 from dagster_dbt import load_assets_from_dbt_project, dbt_cli_resource
 import os
 
@@ -11,12 +11,19 @@ else:
 
 dbt_assets = load_assets_from_dbt_project(
     project_dir=DBT_PROJECT_DIR,
-    profiles_dir=DBT_PROFILES_DIR, 
+    profiles_dir=DBT_PROFILES_DIR
 )
+
 
 @asset 
 def my_asset():
     pass 
+
+
+dbt_asset_job = define_asset_job(
+    name="dbt_assets_local2_target", 
+    selection=AssetSelection.groups("mydbt")
+)
 
 @repository
 def dagsterproj():
@@ -24,9 +31,8 @@ def dagsterproj():
         with_resources(
             dbt_assets + [my_asset], 
             resource_defs= {
-                "dbt": dbt_cli_resource.configured(
-                    {"project_dir": DBT_PROJECT_DIR, "profiles_dir": DBT_PROFILES_DIR}
-                )
+                "dbt": dbt_cli_resource.configured({"project-dir": DBT_PROJECT_DIR, "profiles-dir": DBT_PROFILES_DIR})
             }
-        )
+        ), 
+        dbt_asset_job
     ]
